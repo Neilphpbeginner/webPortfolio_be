8a43d822b2d943eedead7eab0705e486f5bffcf6
Neilphpbeginner <lemmer.neil@gmail.com>
2021-02-09 21:12:20 +0200

    Testing something

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var dotenv = require("dotenv");
+var cors = require("cors");
+var PORT = process.env.PORT || 8080;
+
+var emailRouter = require("./routes/emailRouter");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+dotenv.config();
+
+app.use("/email", emailRouter);
+
+app.get("/", (req, res) => {
+	res.send("Hello");
+});
+
+app.listen(PORT, () => {
+	console.log("Server up and running on port no. " + PORT);
+});
---
+++ routes/emailRouter.js
@@ -0,0 +1,38 @@
+const nodemailer = require("nodemailer");
+const router = require("express").Router();
+
+router.post("/sendEmail", (req, res) => {
+  const {
+    enquiryEmailAdress,
+    enquiryEnailSubject,
+    enquiryEmailContent,
+  } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host: "mail.nlemmercv.co.za",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.ADMINEMAIL,
+      pass: process.env.ADMINPASSWORD,
+    },
+  });
+
+  transporter.sendMail(
+    {
+      from: enquiryEmailAdress,
+      to: "lemmer.neil@gmail.com",
+      subject: enquiryEnailSubject,
+      html: enquiryEmailContent,
+    },
+    (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.status(200).send(info);
+      }
+    }
+  );
+});
+
+module.exports = router;
