ad2713507f26c5f947b28fcb518ad084e57b5f9d
Neilphpbeginner <lemmer.neil@gmail.com>
2022-03-05 10:28:11 +0200

    Delete commented out code, for access purposes.

--- routes/emailRouter.js
+++ routes/emailRouter.js
@@ -30,14 +30,6 @@ router.post("/sendEmail", (req, res) => {
       Hi Neil you recieved an enquiry from ${enquiryEmailAdress}. \n
       And the details of the enquiry is. \n
       <h2>${enquiryEmailContent}</h2>`,
-      // auth: {
-      //   user: process.env.ADMINEMAIL,
-      //   refreshToken:
-      //     "1//04zYwjrTBD7MTCgYIARAAGAQSNwF-L9Irc8Q3WYdKMIIcPZZKE6FOsvSeFbtZx2fgm0ndYwbRkdQZXNs0cwwL_Hq6f7xWaLfu84M",
-      //   accessToken:
-      //     "ya29.A0ARrdaM_aRKLGbxOBCc6gks4WWLoQqFezR-9F4KyfhZtRRr0dPOkEbQU71EpQs-5m6qNQHSyfSCNnSPhWX2YfpcE1ZGa0gQQhShpik7IEKFjag2QRtysKTpOz5_IixSc7Cwud3Z2hgytquy_driX0M5P2W5hl",
-      //   expires: 3599,
-      // },
     },
     (error, info) => {
       if (error) {
