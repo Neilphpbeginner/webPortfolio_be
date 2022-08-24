f8e16a8685bf4f88fa9d4c4b2dd2784db943bf5c
Neilphpbeginner <lemmer.neil@gmail.com>
2022-08-24 14:22:13 +0200

    Password update to MongoDB

--- connectionToBD/mongoosConfig.js
+++ connectionToBD/mongoosConfig.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.export = mongoose
   .connect(
-    "mongodb+srv://Neil:kopPxu5GUZNZnWH7@test.gumuz.mongodb.net/futureemployers?retryWrites=true&w=majority"
+    "mongodb+srv://Neil:6UPKSTfUgEaFMWai@test.gumuz.mongodb.net/futureemployers?retryWrites=true&w=majority"
   )
   .catch((error) => {
     console.log(error);
